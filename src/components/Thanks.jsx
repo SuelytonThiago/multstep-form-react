ae564241e5a52d1ae0ad773bcebe8cd80fc66626
Suelyton Thiago <suelytonsouza0202@gmail.com>
2024-01-14 02:39:43 -0300

    project finished

---
+++ src/components/Thanks.jsx
@@ -0,0 +1,36 @@
+import{
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill,
+} from 'react-icons/bs';
+
+import './Thanks.css';
+
+const emojiData = {
+  unsatisfied: <BsFillEmojiFrownFill/>,
+  neutral: <BsFillEmojiNeutralFill/>,
+  satisfied: <BsFillEmojiSmileFill/>,
+  very_satisfied: <BsFillEmojiHeartEyesFill/>,
+};
+
+const Thanks = ({data}) => {
+  return (
+    <div className = "thanks-container">
+      <h2>Falta pouco...</h2>
+      <p>A sua opinião é muito importante, em breve você receberá um cupom com 10% de desconto para a sua próxima compra.</p>
+      <p>Para concluir a avaliação, clique no botão Enviar abaixo.</p>
+      <h3>Aqui está o resumo da sua avaliação {data.name}:</h3>
+      <p className="review-data">
+        <span>satisfação com o produto:</span>
+        {emojiData[data.review]}
+      </p>
+      <p className="review-data">
+        <span>Comentário:</span>
+        {data.comment   }
+      </p>
+    </div>
+  )
+}
+
+export default Thanks
